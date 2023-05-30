7bfaeb09599c6c54a62b8fd444a7e84a6b7121fa
yuichi1614 <miniquickshooter1114@gmail.com>
2023-05-30 19:58:44 +0900

    tet

---
+++ testttt/testttt/testtttApp.swift
@@ -0,0 +1,17 @@
+//
+//  testtttApp.swift
+//  testttt
+//
+//  Created by 新田雄一郎 on 2023/05/30.
+//
+
+import SwiftUI
+
+@main
+struct testtttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
