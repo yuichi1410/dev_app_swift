f0823cb51003a9a96db9e6d72d5c6b1777110dea
yuichi1614 <miniquickshooter1114@gmail.com>
2023-05-30 20:13:49 +0900

    test

---
+++ aaa/aaa/aaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aaaApp.swift
+//  aaa
+//
+//  Created by 新田雄一郎 on 2023/05/30.
+//
+
+import SwiftUI
+
+@main
+struct aaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
